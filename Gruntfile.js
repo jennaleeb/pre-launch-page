19721635409c97ff6297cc28d39ae32867381f0c
jennaleeb <jennaleeblumenthal@gmail.com>
2014-12-29 13:49:18 -0500

    Updated Gruntfile for publishing to web

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,7 +230,7 @@ module.exports = function (grunt) {
           },
           pages: {
             options: {
-              remote: '!!! PASTE YOUR GITHUB REPO URL HERE',
+              remote: 'https://github.com/jennaleeb/pre-launch-page',
               branch: 'gh-pages'
             }
           },
